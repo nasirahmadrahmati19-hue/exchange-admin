8a041c15d9451d2270e2ef85d0eac601136657af
nasirahmadrahmati19-hue <nasirahmadrahmati19@gmail.com>
2026-08-16 11:41:09 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-// ✅ مسیر صحیح: ./components (چون فایل داخل app است)
 import SettingsDrawer from "./components/SettingsDrawer";
 
 export const metadata: Metadata = {
